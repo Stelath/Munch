67f858077e8e69689bf406261221543714d75e36
Alex <korte@clemson.edu>
2024-06-16 12:50:44 -0700

    Initial Commit

---
+++ Munch/MunchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MunchApp.swift
+//  Munch
+//
+//  Created by Alexander Korte on 6/16/24.
+//
+
+import SwiftUI
+
+@main
+struct MunchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
