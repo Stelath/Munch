a8364de9ebfe740c6677074f0d389ee36375df33
Alex <korte@clemson.edu>
2024-07-26 16:01:00 +0900

    Add Backend API support

---
+++ Munch/Models/Circles/Code.swift
@@ -0,0 +1,12 @@
+//
+//  Code.swift
+//  Munch
+//
+//  Created by Alexander Korte on 7/23/24.
+//
+
+struct Code: Identifiable, Codable {
+    let id: String
+    let code: String
+    let circleId: String
+}
